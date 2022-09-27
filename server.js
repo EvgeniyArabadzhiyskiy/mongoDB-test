41d9d3f410f3c26445bd8afe024083f0d13a710e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-09-27 18:20:37 +0300

    commit

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-const  mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const DB_HOST = require("./config");
 
-const DB_HOST = "mongodb+srv://Evgeniy:aveo4292@cluster0.2xg3aur.mongodb.net/goit?retryWrites=true&w=majority"
-
-mongoose.connect(DB_HOST).then(() => console.log("DataBase connect"))
+mongoose.connect(DB_HOST).then(() => console.log("DataBase connect"));
